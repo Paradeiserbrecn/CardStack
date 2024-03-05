ee1a7534035435bfe366f693aac7294e15964b03
Tomassetti <43643522+scarcheek@users.noreply.github.com>
2024-03-05 12:31:54 +0100

    Started working on the actual game

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] public static Object card;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static Card Create()
+    {
+        GameObject newCardObject = Instantiate(card) as GameObject;
+        Card newCard = newCardObject.GetComponent<Card>();
+
+
+
+        return newCard;
+    }
+}
